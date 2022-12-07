9470f8d2b5d2a7e1c4a5342d62d22a9ca9e20f1d
feblesariel <feblesariel@gmail.com>
2022-12-07 04:41:57 -0300

    Complete V2.0

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "alkemy_presupuesto",
+    "database": "presupuesto",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
